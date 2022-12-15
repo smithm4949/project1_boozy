6a6dc7b2cfef291fbaa74e9775d5aac9bf970afd
MEwing <masandraewing@gmail.com>
2022-12-15 11:19:07 -0500

    Building on homepage with buttons and functionality

--- assets/masandra.js
+++ assets/masandra.js
@@ -3,14 +3,40 @@ $('#getStarted').click(function (e) {
     console.log("I Work");
     $('#ingredientSection').attr("style", "display:block");
     $('#introCard').attr("style", "display:none");
+    localStorage.clear();
 });
 
 $('#goButton').click(function (e) {
     e.preventDefault();
-    fetchIngredient();
+    displayIngredient();
 });
 
-function fetchIngredient() {
-    var userInputIngEl = $('#userInputIng').val();
-    console.log(userInputIngEl);
+$('#clearList').click(function (e) {
+    e.preventDefault();
+    localStorage.clear();
+});
+
+
+function displayIngredient() {
+        var userInputIngEl = $('#userInputIng').val();
+        var ingredientListEl = $("#ingredientList");
+        var newIngBtn = $('<li class="item button is-success"><span class="icon is-small"><i class="fas fa-check"></i></span></li>');
+        newIngBtn.appendTo(ingredientListEl);
+        newIngBtn.text(userInputIngEl);
+        newIngBtn.val(userInputIngEl);
+        saveIngredient();
+// !TO DO - Look up method to remove the duplicate ingredient buttons
+
+    function saveIngredient (){
+        var oldItems = JSON.parse(localStorage.getItem("Ingredients")) || [];
+        var newItem = $('#userInputIng').val().toLowerCase();
+        // Use the running list object and add on new items to the old items
+        oldItems.push(newItem);
+    
+        // Remove duplicates so cities are not repeating in the buttons of previously searched cities history
+        var uniqueItems = [...new Set(oldItems)];
+        // Store the unique items - no duplicates
+        localStorage.setItem("Ingredients", JSON.stringify(uniqueItems));  
+        $('#userInputIng').val("");   
+    };
 };
